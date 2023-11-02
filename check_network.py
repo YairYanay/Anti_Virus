43c5562e5144997faa3eb0754d749cd8f10184d2
YairYanay <108800992+YairYanay@users.noreply.github.com>
2023-11-02 20:12:58 +0200

    thread which get data always
    create thread that check all time when get data and print it.
    convert from UDP to TC because UDP cant send length odd string and TCP can.

--- check_network.py
+++ check_network.py
@@ -3,6 +3,14 @@ import psutil
 import time
 #pip install speedtest-cli
 import speedtest
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)#pip install scapy
+from scapy.all import *
+import threading
+# Use Npcap for packet capture
+#conf.use_pcap = True
+#python C:\anti-virus\check_network.py
+
 
 def speedtest_networks():
     st = speedtest.Speedtest()
@@ -10,10 +18,35 @@ def speedtest_networks():
     print("Download Speed:", max_speed) # 1e6 = 1M
     return max_speed
 
-def check_live_usage_network_my_pc(max_network_data):
+
+def send_network_brodcust_packet(message):
+    udp_brodcust_packet = IP(dst="255.255.255.255") / TCP(sport=8202) / message
+    send(udp_brodcust_packet, verbose=False)
+
+
+def custom_lfilter(packet):
+    # Define the custom filter function
+    if IP in packet and TCP in packet:
+        if packet[IP].dst == "255.255.255.255" and packet[TCP].sport == 8202 and packet.haslayer(Raw):
+            return True
+    return False
+        
+
+def recv_network_brodcust_packet():
+    while True:
+        packet_sniff = sniff(lfilter=custom_lfilter, count=1, timeout=1)
+        if len(packet_sniff) != 0 and packet_sniff[0].load.decode().split(':')[0] == 'NTWK': 
+            packet_data = packet_sniff[0].load.decode().split(':')[1].split(',')
+            print(f"Network Usage opponent: {packet_data[0]} Mbps ({packet_data[1]}%)")
+
+
+def check_live_usage_network_my_pc(max_network_data, reset_thread_recv=False):
     # Initialize variables to track previous network stats
     previous_net_stats = psutil.net_io_counters()
-
+    
+    t = threading.Thread(target=recv_network_brodcust_packet)
+    t.start()
+    
     while True:
         # Get the current network statistics
         current_net_stats = psutil.net_io_counters()
@@ -24,19 +57,32 @@ def check_live_usage_network_my_pc(max_network_data):
         
         # Calculate the network usage as a percentage
         total_bytes_diff = bytes_sent_diff + bytes_recv_diff
-        network_usage = (total_bytes_diff / 1e6) * 8  # Convert to Mbps, 1e6 == 1M
+        network_usage = int(round((total_bytes_diff / 1e6) * 8, 2))  # Convert to Mbps, 1e6 == 1M
+        precent_usage = int(round(network_usage / max_network_data * 100 , 0))
         
-        print(f"Network Usage: {int(round(network_usage, 0))} Mbps ({int((round(network_usage / max_network_data * 100 , 0)))}%)")
+        print(f"Network Usage: {network_usage} Mbps ({precent_usage}%)")
+        
+        #send and recv data
+        send_network_brodcust_packet(f"NTWK:{network_usage},{precent_usage}")
+        if reset_thread_recv:
+            t.join()
+            t = threading.Thread(target=recv_network_brodcust_packet)
+            t.start()
+            reset_thread_recv = False
+
         
         # Update the previous network stats for the next measurement
         previous_net_stats = current_net_stats
         
         time.sleep(1)
+    
+    t.join()
 
 
 def fix_network_problems():
     pass
 
+
 if __name__ == '__main__':
-    max_network_data = speedtest_networks()
-    check_live_usage_network_my_pc(max_network_data)
+    #max_network_data = speedtest_networks()
+    check_live_usage_network_my_pc(500)
